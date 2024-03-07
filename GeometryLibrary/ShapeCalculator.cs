4579b2e4a12992d3024adb1f8066876465ad08f6
timisev <101486611+timisev@users.noreply.github.com>
2024-03-07 19:55:58 +0300

    first commit

---
+++ GeometryLibrary/ShapeCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace GeometryLibrary
+{
+    public class ShapeCalculator
+    {
+        public static double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double CalculateTriangleArea(double side1, double side2, double side3)
+        {
+            double s = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        }
+
+        public static bool IsRightTriangle(double side1, double side2, double side3)
+        {
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+
+            return sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2];
+        }
+    }
+}
