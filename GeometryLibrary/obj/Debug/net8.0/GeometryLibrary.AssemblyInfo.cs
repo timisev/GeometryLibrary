dc309a1c311291f351ab4432d3ad1e14e360b499
timisev <101486611+timisev@users.noreply.github.com>
2024-04-27 09:25:00 +0300

    first commit

--- GeometryLibrary/obj/Debug/net8.0/GeometryLibrary.AssemblyInfo.cs
+++ GeometryLibrary/obj/Debug/net8.0/GeometryLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeometryLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4579b2e4a12992d3024adb1f8066876465ad08f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeometryLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeometryLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
